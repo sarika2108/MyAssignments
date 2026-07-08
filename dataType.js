78a51bf38f49968261f237ccc3a59e4e07a4400e
sarika2108 <42716258+sarika2108@users.noreply.github.com>
2026-07-08 10:11:36 +0530

    Add files via upload
    day1

---
+++ dataType.js
@@ -0,0 +1,20 @@
+
+var firstName='sarika'
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName='org'
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileName=99897878
+console.log(mobileName);
+console.log(typeof mobileName);
+
+var isAutomated=true
+console.log(isAutomated);
+console.log(typeof isAutomated);
+
+var hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
