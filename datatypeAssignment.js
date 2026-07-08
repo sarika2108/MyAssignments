fdf9fa3b412957bfabc29870c9c8028bf18f03a5
sarika2108 <42716258+sarika2108@users.noreply.github.com>
2026-07-08 11:03:28 +0530

    Add files via upload
    homeAssignment

---
+++ datatypeAssignment.js
@@ -0,0 +1,19 @@
+let userName='sarika'
+console.log(userName);
+console.log(typeof userName);
+
+let organization='org'
+console.log(organization);
+console.log(typeof organization);
+
+let contactNumber=99897878
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+let knowsAutomation=true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
